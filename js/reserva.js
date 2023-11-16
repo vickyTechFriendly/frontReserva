767d2ae4bea00a8482be93098356e359555b4a1c
vickyTechFriendly <victoria@techfriendly.es>
2023-11-16 14:34:15 +0100

    no está devolviendo las horas de reserva de los días que no sean el actual. Sigo mirándolo

--- js/reserva.js
+++ js/reserva.js
@@ -1,5 +1,6 @@
 async function getBuilding(edificioId) {
-  function obtenerDateTimeGet(isStart) {
+  
+  function obtenerFechaActual(isStart) {
     const fecha = new Date();
     const offset = isStart ? 0 : 6; // 0 para inicio, 6 para fin
     fecha.setDate(fecha.getDate() + offset);
@@ -10,13 +11,10 @@ async function getBuilding(edificioId) {
 
     const dateTimeGet = `${año}/${mes}/${dia}T00:00:00`;
 
-    return dateTimeGet;
+    return dateTimeGet; //aquí devolvemos la fecha actual en formato yyyy/mm/ddThh:mm:ss
   }
-  const startDateTimeGet = obtenerDateTimeGet(true);
-  const endDateTimeGet = obtenerDateTimeGet(false);
-
-  console.log(startDateTimeGet);
-  console.log(endDateTimeGet);
+  const startDateTimeGet = obtenerFechaActual(true); 
+  const endDateTimeGet = obtenerFechaActual(false);
 
   function convertirFecha(inputFecha) {
     const fecha = new Date(inputFecha);
@@ -24,7 +22,7 @@ async function getBuilding(edificioId) {
     const mes = String(fecha.getMonth() + 1).padStart(2, "0");
     const dia = String(fecha.getDate()).padStart(2, "0");
 
-    return `${año}-${mes}-${dia}`;
+    return `${año}-${mes}-${dia}`; //aquí convertimos la fecha al formato yyyy-mm-dd para poder mostrarla en el input
   }
 
   try {
@@ -35,7 +33,7 @@ async function getBuilding(edificioId) {
         headers: {
           "Content-Type": "application/json",
           "X-Booked-SessionToken":
-            "3f038fac440d3521b569493a5b5340fb50335c6028c3d5c9ef",
+            "46c566ad7c8df48da0d682a354c337df485a4303136cfc370a",
           "X-Booked-UserId": "1",
         },
       }
@@ -47,6 +45,7 @@ async function getBuilding(edificioId) {
     }
     const building = await response.json();
     console.log("buildingActual", building);
+    actualizarTitulo(edificioId);
 
     if (building && building.dates.length > 0) {
       const firstDate = building.dates[0];
@@ -68,6 +67,9 @@ async function getBuilding(edificioId) {
         document.getElementById("resourceId").appendChild(option);
       });
 
+      //Agregar un listener al cambio de fecha
+      startDateTimeInput.addEventListener("change", actualizarHoras);
+
       // Agrega un evento de cambio al select de resourceId
 const resourceIdSelect = document.getElementById("resourceId");
 resourceIdSelect.addEventListener("change", mostrarHorasDisponibles);
@@ -84,6 +86,7 @@ function mostrarHorasDisponibles() {
   const horasDisponibles = building.dates[0].resources.find(
     (resource) => resource.resourceId === selectedResourceId
   ).slots.filter((slot) => slot.isReservable);
+  console.log("horasDisponibles", horasDisponibles);
 
   // Agregar las nuevas opciones al select de horas
   horasDisponibles.forEach((slot) => {
@@ -95,6 +98,50 @@ function mostrarHorasDisponibles() {
   });
 }
 
+function actualizarHoras() {
+  try {
+    // Obtener la nueva fecha del input
+    let nuevaFecha = document.getElementById("startDateTime").value;
+    nuevaFecha = `${nuevaFecha}T00:00:00+0100`;
+
+    // Limpiar las opciones actuales del select de horas
+    const horaSelect = document.getElementById("hora");
+    horaSelect.innerHTML = '<option value="" selected disabled>Selecciona una hora</option>';
+
+    // Obtener el valor seleccionado del select de resourceId
+    const selectedResourceId = resourceIdSelect.value;
+
+    // Buscar las horas disponibles para la sala y la nueva fecha seleccionada
+    const dateData = building.dates.find(
+      (date) => date.date === nuevaFecha
+    );
+
+    if (dateData && dateData.resources) {
+      const resource = dateData.resources.find(
+        (resource) => resource.resourceId === selectedResourceId
+      );
+
+      if (resource && resource.slots) {
+        const horasDisponibles = resource.slots.filter((slot) => slot.isReservable);
+
+        // Agregar las nuevas opciones al select de horas
+        horasDisponibles.forEach((slot) => {
+          const option = document.createElement("option");
+          const formattedTime = slot.startDateTime.split("T")[1].substring(0, 5); // Extraer hh:mm de la cadena
+          option.value = slot.slotId;
+          option.innerText = formattedTime;
+          horaSelect.appendChild(option);
+        });
+      } else {
+        console.warn(`No hay información disponible para la sala y fecha seleccionadas`);
+      }
+    } else {
+      console.warn(`No hay información disponible para la fecha ${nuevaFecha}`);
+    }
+  } catch (error) {
+    console.error("Error al actualizar horas:", error);
+  }
+}
       return building;
     } else {
       throw new Error(
@@ -107,24 +154,61 @@ function mostrarHorasDisponibles() {
   }
 }
 
+function actualizarTitulo(edificioId) {
+  const titulo = document.getElementById("salaActual");
+
+  switch (edificioId) {
+    case 1:
+      titulo.innerText = "C.C. Bellavista";
+      break;
+    case 2:
+      titulo.innerText = "Espacio Santa Clara";
+      break;
+    default:
+      titulo.innerText = "Edificio Desconocido";
+  }
+}
 
 
-getBuilding(1);
+
+getBuilding(2);
+
+
+/* const duracionReserva = building.dates[0].resources[0].slots.filter(slot => slot.isReservable);
+const horasDisponibles = building.dates[0].resourcesslots.filter((slot) => slot.isReservable); */
+
+
 
 /////CREAR RESERVA//////
 document
   .getElementById("reservationForm")
   .addEventListener("submit", function (event) {
     event.preventDefault();
-    const description = document.getElementById("description").value;
-    const startReservationDateTime =
-      document.getElementById("startDateTime").value;
-    const endDateTime = document.getElementById("endDateTime").value;
-    const title = document.getElementById("title").value;
-    const allowParticipation =
-      document.getElementById("allowParticipation").value;
-    const termsAccepted = document.getElementById("termsAccepted").value;
+
     const resourceId = parseInt(document.getElementById("resourceId").value);
+    const startReservationDateT = document.getElementById("startDateTime").value;
+    const startReservationTime = document.getElementById("hora").value;
+    const startReservationDateTime = `${startReservationDateT}T${startReservationTime}:00+0100`;
+    const endDateTime = document.getElementById("endDateTime").value;
+    
+    const attributeId = document.getElementById("attributeId").value;
+    const attributeValue = document.getElementById("attributeValue").value;
+    const customAttributes = [
+      {
+        attributeId: parseInt(attributeId),
+        attributeValue: attributeValue,
+      },
+    ];
+
+    const description = document.getElementById("description").value;
+    
+    const title = document.getElementById("title").value;
+    
+    
+
+    const allowParticipation = document.getElementById("allowParticipation").value;
+    const termsAccepted = document.getElementById("termsAccepted").value;
+    
 
     const accessories = [];
     for (let i = 1; i <= 4; i++) {
@@ -146,6 +230,7 @@ document
       startDateTime: startReservationDateTime,
       endDateTime: endDateTime,
       resourceId: resourceId,
+      customAttributes: customAttributes,
       title: title,
       userId: 1,
       allowParticipation: allowParticipation,
@@ -159,7 +244,7 @@ document
       headers: {
         "Content-Type": "application/json",
         "X-Booked-SessionToken":
-          "3f038fac440d3521b569493a5b5340fb50335c6028c3d5c9ef",
+          "46c566ad7c8df48da0d682a354c337df485a4303136cfc370a",
         "X-Booked-UserId": "1",
       },
       body: JSON.stringify(reservationData),
