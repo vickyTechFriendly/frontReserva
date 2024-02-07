628e227614989d317c9d6bcd8a9d8c2cd4c136e3
vickyTechFriendly <victoria@techfriendly.es>
2024-02-07 14:55:50 +0100

    refactorizando ciertas funciones

--- js/reserva_ok.js
+++ js/reserva_ok.js
@@ -75,20 +75,62 @@ function actualizarTitulo(edificioId) {
   }
 }
 
+function mostrarHorasDisponibles(e, date) {
+  // Limpiar las opciones actuales del select de horas
+  const horaSelect = document.getElementById("hora");
+  horaSelect.innerHTML = '<option value="" selected disabled>Selecciona una hora</option>';
+  
+  // Obtener el valor seleccionado del select de resourceId
+  const selectedResourceId = e.target.value;
+  
+  //obtener hora actual para comparar con las horas disponibles
+  const horaActual = new Date()
+  
+  // Buscar las horas disponibles para la sala seleccionada
+  const horasDisponibles = date.resources.find(
+  (resource) => resource.resourceId === selectedResourceId
+  ).slots.filter((slot) => {
+  const startDateTime = new Date(slot.startDateTime);
+  return slot.isReservable && startDateTime > horaActual;
+  });
+  
+  // Agregar las nuevas opciones al select de horas
+  horasDisponibles.forEach((slot) => {
+  const option = document.createElement("option");
+  const formattedTime = slot.startDateTime.split("T")[1].substring(0, 5); // Extraer hh:mm de la cadena
+  option.value = slot.startDateTime;
+  option.dataset.endDateTime = slot.endDateTime; // Guardar la fecha de fin en un atributo personalizado del elemento
+  option.innerText = formattedTime;
+  horaSelect.appendChild(option);
+  });
+  
+  //Actualizar el valor del input de endDateTime
+  horaSelect.addEventListener('change', function() {
+  const selectedOption = this.options[this.selectedIndex];
+  const endDateTime = selectedOption.dataset.endDateTime; // Recupera el endDateTime del dataset
+  document.getElementById('endDateTime').value = endDateTime; // Actualiza el valor del input oculto
+  //console.log("endDateTimeInput",endDateTime);
+  });
+  }
+
 function procesarFechasYRecursos(building) {
   let firstDate = building.dates[0];
   const firstFecha = firstDate.date; //primera fecha devuelta por la API
 
-  // Mostrar la primera fecha en el input de fecha
+  // Listener en el campo Date + Mostrar la primera fecha en el input de fecha
   const startDateTimeInput = document.getElementById("startDateTime");
   startDateTimeInput.value = convertirFecha(firstFecha); //convierte la primera fecha al formato yyyy-mm-dd para mostrarla en el input
-  startDateTimeInput.addEventListener("change", (e) => { //agrega un evento de cambio al input de fecha para mostrar las salas disponibles para esa fecha en el select de salas
-    const nuevaFecha = e.target.value;
-    firstDate = building.dates.find((date) => date.date.split("T")[0] === nuevaFecha);
+  startDateTimeInput.addEventListener("change", (e) => { 
+  //Busca la fecha seleccionada en el array de fechas devuelto por la API -que devuelve los primeros 7 desde la fecha actual-
+  const nuevaFecha = e.target.value; 
+  firstDate = building.dates.find((date) => date.date.split("T")[0] === nuevaFecha);
+  mostrarHorasDisponibles({ target: resourceIdSelect }, firstDate);
+});
 
-     });
+  // Listener en el select + Mostrar nombres salas en las opciones del select
+  const resourceIdSelect = document.getElementById("resourceId");
+  resourceIdSelect.addEventListener("change",(e)=> mostrarHorasDisponibles(e,firstDate));
 
-  // Mostrar nombres de salas en el select de salas 
   firstDate.resources.forEach((resource, resourceIndex) => {
     const resourceName = resource.resourceName;
     const resourceId = resource.resourceId;
@@ -99,60 +141,9 @@ function procesarFechasYRecursos(building) {
     document.getElementById("resourceId").appendChild(option); 
   });
 
-// Agrega un evento de cambio al select de resourceId
-const resourceIdSelect = document.getElementById("resourceId");
-resourceIdSelect.addEventListener("change",(e)=> mostrarHorasDisponibles(e,firstDate));
-
-// Agrega un evento de cambio al input de fecha
-startDateTimeInput.addEventListener("change", (e) => {
-  const nuevaFecha = e.target.value;
-  firstDate = building.dates.find(
-    (date) => date.date.split("T")[0] === nuevaFecha
-  );
-  mostrarHorasDisponibles({ target: resourceIdSelect }, firstDate);
-}
-);
-
-function mostrarHorasDisponibles(e, date) {
-// Limpiar las opciones actuales del select de horas
-const horaSelect = document.getElementById("hora");
-horaSelect.innerHTML = '<option value="" selected disabled>Selecciona una hora</option>';
-
-// Obtener el valor seleccionado del select de resourceId
-const selectedResourceId = e.target.value;
-
-//obtener hora actual para comparar con las horas disponibles
-const horaActual = new Date()
-
-// Buscar las horas disponibles para la sala seleccionada
-const horasDisponibles = date.resources.find(
-(resource) => resource.resourceId === selectedResourceId
-).slots.filter((slot) => {
-const startDateTime = new Date(slot.startDateTime);
-return slot.isReservable && startDateTime > horaActual;
-});
-
-// Agregar las nuevas opciones al select de horas
-horasDisponibles.forEach((slot) => {
-const option = document.createElement("option");
-const formattedTime = slot.startDateTime.split("T")[1].substring(0, 5); // Extraer hh:mm de la cadena
-option.value = slot.startDateTime;
-option.dataset.endDateTime = slot.endDateTime; // Guardar la fecha de fin en un atributo personalizado del elemento
-option.innerText = formattedTime;
-horaSelect.appendChild(option);
-});
-
-//Actualizar el valor del input de endDateTime
-horaSelect.addEventListener('change', function() {
-const selectedOption = this.options[this.selectedIndex];
-const endDateTime = selectedOption.dataset.endDateTime; // Recupera el endDateTime del dataset
-document.getElementById('endDateTime').value = endDateTime; // Actualiza el valor del input oculto
-//console.log("endDateTimeInput",endDateTime);
-});
-}
 }
 
-getBuilding(1);
+getBuilding(2);
 
 /////CREAR RESERVA//////
 document.getElementById('quantity1').addEventListener('change', function() { //Neesario que esté fuera del evento submit para que se actualie el valor del checkbox
